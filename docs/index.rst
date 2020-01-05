7ac8cdcf239a4d7cbae0a598e0afb09c2f579d5a
Avi Alkalay <avi@unix.sh>
2020-01-05 03:10:02 -0300

    preparing for documentation

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. iOSbackup documentation master file, created by
+   sphinx-quickstart on Sun Jan  5 03:07:40 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to iOSbackup's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="iOSbackup-avibrazil", # Replace with your own username
+    version="0.9.0",
+    author="Avi Alkalay",
+    author_email="avibrazil@gmail.com",
+    description="Reads and extracts files from password-encrypted iOS backups",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avibrazil/iOSbackup",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: MacOS X",
+        "Environment :: Console",
+        "Environment :: Win32 (MS Windows)"
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Legal Industry",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: MacOS",
+        "Operating System :: iOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Topic :: Database",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Embedded Systems",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Archiving :: Backup",
+        "Topic :: System :: Recovery Tools"
+    ],
+    python_requires='>=3.6',
+)
