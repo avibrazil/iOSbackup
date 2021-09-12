d38e415b9895f432f9af6449631e31ffdc60ce30
Avi Alkalay <avibrazil@gmail.com>
2021-09-11 23:24:11 -0400

    version bump

--- iOSbackup/__init__.py
+++ iOSbackup/__init__.py
@@ -23,7 +23,7 @@ except:
     from Crypto.Cipher import AES # https://www.dlitz.net/software/pycrypto/
 
 
-__version__ = '0.9.92'
+__version__ = '0.9.921'
 
 module_logger = logging.getLogger(__name__)
 
