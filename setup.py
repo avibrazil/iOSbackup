b99a537c41352d30afe1588e012cf2dd53c16466
Kolen Cheung <christian.kolen@gmail.com>
2021-09-23 19:10:56 -0700

    explicit encoding

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from iOSbackup import __version__
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
